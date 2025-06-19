c052e5f220cf7e3d3901613d06566c4ea190f457
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-06-18 23:26:27 -0400

    make start script

--- py-server/start.sh
+++ py-server/start.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "START SCRIPT TRIGGERED" >> log.txt
+
+# Start in bridge script directory (Python lifx bridge)
+cd /home/admin_dollhouse/dollhouse-salma/bridge-script || exit 1
+
+# Start the lifx bridge
+venv/bin/python3 main.py &
+
+# Start the Pure Data patch
+cd /home/admin_dollhouse/dollhouse-salma/pd || exit 1
+pd -nogui -noadc -alsa -audiooutdev 4 main.pd &
+
+# Log
+echo "START SCRIPT TRIGGERED DOLL $(date)" >> /home/admin_dollhouse/dollhouse-salma/bridge-script/log.txt
