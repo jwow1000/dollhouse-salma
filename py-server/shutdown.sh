93cbbbaca8694a640e5739d9910c3b4de307e163
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-06-18 23:48:32 -0400

    update shutdown sh

--- py-server/shutdown.sh
+++ py-server/shutdown.sh
@@ -6,11 +6,11 @@ pkill -f "pd.*main.pd"
 # Stop lifx bridge script
 pkill -f "venv/bin/python3.*main.py"
 
-# Optional: Stop Flask service (if started by systemd)
+# Stop Flask service (started by systemd)
 sudo systemctl stop dollhouse-web.service
 
 # Log shutdown
 echo "SHUTDOWN SCRIPT TRIGGERED $(date)" >> /home/admin_dollhouse/dollhouse-salma/bridge-script/log.txt
 
 # Shutdown the Pi safely
-sudo /sbin/shutdown -h now
+sudo shutdown -h now
